5f334467cad47a44a00322735e59a9ac78222087
Kaneisha <minnieneisha@fullsail.edu>
2014-01-28 17:59:00 -0500

    comments done

--- CheerNation/js/main.js
+++ CheerNation/js/main.js
@@ -1,6 +1,112 @@
-$('#volume').click(function(){
+var users = '';
+var chatRef = new Firebase('https://cheernation.firebaseio.com');
+var auth = new FirebaseSimpleLogin(chatRef, function(error, user){
+	if(error){
+		console.log(error)
+	}else if(user){
+		users = user;
+
+		loggedIn();
+	}else{
+		$('#git').click(function(){
+			auth.login('github');
+		});
+
+		$('#twitter').click(function(){
+			auth.login('twitter');
+		});
+
+		$('#logout').hide();
+	}
+});
+
+$('#logout').click(function() {
+        auth.logout();
+        $('#buttons').show();
+        $('nav p').hide();
+});
+
+var loggedIn = function(){
+	console.log('user');
+	if(users.provider === 'twitter'){
+		image = users.profile_image_url;
+		console.log(users.profile_image_url);
+	}
+	if(users.provider === 'github'){
+		image = users.avatar_url;
+	}
+	$('#buttons').hide();
+	$('#logout').show();
+	$('nav p').html('Welcome, ' + users.username);
+
+};
+
+$('#submit').on('click', function(){
+	var comment = $('#comments_input').val();
+	var user_comment = users.username;
+
+	if(users.provider === 'twitter'){
+		var avi = users.profile_image_url;
+	}
+	if(users.provider === 'github'){
+		var avi = users.avatar_url;
+	}
+
+	if(users != '' && comment != ''){
+		chatRef.push({
+			name: user_comment,
+			text: comment,
+			image: avi
+		});
+		$('#comments_input').val('');
+		console.log('hey');
+	}
+
+});
+
+chatRef.on('child_added', function(snapshot){
+	var message = snapshot.val();
+	//displayChatMessage(message.comment, message.user_comment, message.avi);
+
+	console.log(message.image);
+
+	$('#comments').append('<div id="username"> <img src="' + message.image + '">' + '<h1 id="user">' + message.name + '</div>' + '<p>' + message.text + '</p>' );
+
+	//$('#comment_contain')[0].scrollTop = $('#comment_contain')[0].scrollHeight;
+});
+
+	// function displayChatMessage(comment,user_comment,avi){
+	// //$('#comments').append('<img src="' + chat.avi + '">');
+	// $('#comments').append('<div id="username"> <h1 id="user">' + user_comment + '</div>' + '<p>' + comment + '</p>' );
+	// //$('#user_comment').html(comment);
+	// console.log('comments: ' + comment);
+	// console.log('go');
+	//  };
+
+
+
+
+
+
+//--------------------------------- Video Player --------------------------------------//
+
+$("#vol").css("display","none");
+$('#volume').on('mouseover', function(){
 	$("#vol").css("display","block");
 });
+
+$('#volume').on('mouseout', function(){
+	$("#vol").css("display","none");
+});
+
+$('#vol').on('mouseover', function(){
+	$("#vol").css("display","block");
+});
+
+$('#vol').on('mouseout', function(){
+	$("#vol").css("display","none");
+});
+
 
 var camClick = false;
 var showCam = false;
@@ -15,6 +121,8 @@ var playing = false;
 var recording = false;
 var rec_tog = false;
 var camTog = false;
+var xPos = 0;
+var userMove = false;
 
 	var flashReady = function(){
 		
@@ -78,14 +186,14 @@ var camTog = false;
 
         }); 
 
-        $("#microphone").on('click', function(){
+        $("#microphone").on('mouseover', function(){
         // $("#microphone").css('margin-left', '-130px');
         //$("#play").css('margin-left', '-270px');
         //$("#cam").css('margin-left', '-150px');
         $("#bar").css('position', 'absolute');
         $("#bar").css('top', '448px');
         $("#bar").css('left', '700px');
-         $("#ball").css('z-index', '1');
+        $("#ball").css('z-index', '1');
         $("#ball").css('position', 'absolute');
         $(".micro_menu li").css('background-color', '#b1b1b1;')
             micClick = true;
@@ -98,6 +206,13 @@ var camTog = false;
             };
 
         }); 
+
+        $("#microphone").on('mouseout', function(){
+        	$(".micro_menu li").hide();
+        });
+        $(".micro_menu li").on('mouseover', function(){
+        	$(".micro_menu li").css("display", "block");
+        });
 
        $("#rec").on('click', function(){
        		playing = false;
@@ -154,55 +269,42 @@ var camTog = false;
 		//var duration = 10;
 		var moving = true;
 		
-
-		if(moving){
-			xPos = (seek / vidDuration) * 276;
-			//$('#ball').css('left', xPos + 680);
-			$('#ball').offset({left: xPos + 730});
+		xPos = (seek / vidDuration) * 276;
+		// if(moving){
+		// 
+		// 	//$('#ball').css('left', xPos + 680);
+		// 	$('#ball').offset({left: xPos + 730});
 			
-		};
+		// };
+
+		if(!userMove){
+			//Gets position of video ansd moves scrubber 
+			$('#ball').offset({left: $('#bar').offset().left + xPos + 15});
+		}
 	};
-
-	var userMove = false;
-	// $('#ball').on('mousedown', function(e){
-	// 			console.log('clicked');
-	// 			userMove = true;
-	// 			if(e.pageX < $('#ball').offset().left + 730){
-	// 				$('#ball').offset({left: e.pageX});
-	// 				}
-	// 				e.preventDefault();
-	// 		});
-				
-
-	// 		$('#ball').on('mouseup', function(e){
-	// 			var currentPos = ($('#ball').position().left / 730) * vidDuration;
-
-	// 			flash.setTime(currentPos);
-	// 		});
 
 	$('#ball').on('mousedown', function(e){
 		userMove = true;
 		seekMover();
-		// if(e.pageX < $('#ball').offset().left + 730){
-		// 		$('#ball').offset({left: e.pageX});
-		// 	}
-		// 	e.preventDefault();
+		e.preventDefault();
+	
 	});
-	$('#ball').on('mouseup', function(e){
+	$(document).on('mouseup', function(e){
 		userMove = false;
 	});
 
-	var seekMover = function(){
-		$('#ball').on('mousemove', function(e){
-			// if(e.pageX < $('#ball').offset().left + 730){
-			// 	
-			// }
+	function seekMover(){
+		
+		$(document).on('mousemove', function(e){
+			var new_time = ((e.pageX - $('#bar').offset().left) / 276) * vidDuration;
 			if(userMove){
 				$('#ball').offset({left: e.pageX});
+
+				flash.setTime(new_time);
 			}
 			e.preventDefault();
 		});
-	}
+	};
 
 			
 	var getDuration = function(duration){
@@ -224,7 +326,7 @@ var camTog = false;
 
 		if(showCam){
 			for(var i = 0, max = cams.length; i < max; i++){
-				$('ul#camera').append('<li data-id="'+i+'">' + cams[i] + '</li>');
+				$('ul#camera').append('<li data-id="'+i+'"><a>' + cams[i] + '</a></li>');
 				console.log(cameraPick);
 			}
 
@@ -267,11 +369,11 @@ var camTog = false;
 			flash.startRecording('work', cameraPick, micPick);
 			recording = true;
 			console.log(recording);
-			console.log("hi");
 		}else{
 			flash.stopRecording();
 			//$('#rec').empty();
 			recording = false;
+			console.log('stop recording',recording);
 		}
 
 		if(!rec_tog){
@@ -283,6 +385,7 @@ var camTog = false;
  				flash.stopRecording();
  				// $('#rec').empty();
  				recording = false;
+ 				console.log('stop recording plz',recording);
  			};
 
 	}
